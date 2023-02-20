083868008e5fa3fe6818ba04d99c0374ad8469db
marieagrapart <marie.agrapart@gmail.com>
2023-02-20 12:14:08 +0000

    blake

--- 1-API_weather.py
+++ 1-API_weather.py
@@ -1,74 +1,78 @@
 # libraries import
 
-import pandas as pd 
+import pandas as pd
 import requests
 import json
 from pandas import json_normalize
 import time
 from datetime import datetime
-import plotly.io as pio
-import plotly.express as px
-import plotly.graph_objects as go
 from typing import Any, Dict, List
 import boto3
 import os
 
 # city list given by the exercice
 
-city_list = ["Mont Saint Michel",
-"St Malo",
-"Bayeux",
-"Le Havre",
-"Rouen",
-"Paris",
-"Amiens",
-"Lille",
-"Strasbourg",
-"Chateau du Haut Koenigsbourg",
-"Colmar",
-"Eguisheim",
-"Besancon",
-"Dijon",
-"Annecy",
-"Grenoble",
-"Lyon",
-"Gorges du Verdon",
-"Bormes les Mimosas",
-"Cassis",
-"Marseille",
-"Aix en Provence",
-"Avignon",
-"Uzes",
-"Nimes",
-"Aigues Mortes",
-"Saintes Maries de la mer",
-"Collioure",
-"Carcassonne",
-"Ariege",
-"Toulouse",
-"Montauban",
-"Biarritz",
-"Bayonne",
-"La Rochelle"]
+city_list = [
+    "Mont Saint Michel",
+    "St Malo",
+    "Bayeux",
+    "Le Havre",
+    "Rouen",
+    "Paris",
+    "Amiens",
+    "Lille",
+    "Strasbourg",
+    "Chateau du Haut Koenigsbourg",
+    "Colmar",
+    "Eguisheim",
+    "Besancon",
+    "Dijon",
+    "Annecy",
+    "Grenoble",
+    "Lyon",
+    "Gorges du Verdon",
+    "Bormes les Mimosas",
+    "Cassis",
+    "Marseille",
+    "Aix en Provence",
+    "Avignon",
+    "Uzes",
+    "Nimes",
+    "Aigues Mortes",
+    "Saintes Maries de la mer",
+    "Collioure",
+    "Carcassonne",
+    "Ariege",
+    "Toulouse",
+    "Montauban",
+    "Biarritz",
+    "Bayonne",
+    "La Rochelle",
+]
 
 # request for lat & long for each city
 
 position_cities = {}
 
 for city in city_list:
-    resp =  requests.get(f"https://nominatim.openstreetmap.org/?q={city}&format=json&countrycodes=1974&limit=1").json()
-    lat = resp[0]['lat']
-    long = resp[0]['lon']
-    position_cities[city] = [lat,long]
+    resp = requests.get(
+        f"https://nominatim.openstreetmap.org/?q={city}&format=json&countrycodes=1974&limit=1"
+    ).json()
+    lat = resp[0]["lat"]
+    long = resp[0]["lon"]
+    position_cities[city] = [lat, long]
 
-position_cities
 
-# request for the weather for each city 
+# request for the weather for each city
+
 
 def get_weather_forecasts(latitude, longitude):
-    resp =  requests.get(f"http://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid=c57023819bfd474995f3bf1b69e50c0d")
+    resp = requests.get(
+        f"http://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&appid=c57023819bfd474995f3bf1b69e50c0d"
+    )
     resp.raise_for_status()
-    return resp.json()['list']
+    return resp.json()["list"]
+
 
 # Keep usefull information, in our case I choose the precipiration & the temperature
 
@@ -77,65 +81,92 @@ chosen_weather = []
 for city, (latitude, longitude) in position_cities.items():
     weather_forecasts = get_weather_forecasts(latitude, longitude)
 
-    for forecast in weather_forecasts: 
-        dt_object = datetime.utcfromtimestamp(forecast['dt'])
-        main_weather = forecast['weather'][0]['main']
-        prepcipitation = forecast['pop']
-        temperature = forecast['main']['temp'] - 273.15 # convertion K to C
-    
-        chosen_weather.append({'city' : city,
-                        'dt_object': dt_object,
-                        'latitude': latitude,
-                        'longitude': longitude,
-                        'main_weather': main_weather, 
-                        'prepcipitation' :prepcipitation,
-                        'temperature' :temperature})
+    for forecast in weather_forecasts:
+        dt_object = datetime.utcfromtimestamp(forecast["dt"])
+        main_weather = forecast["weather"][0]["main"]
+        precipitation = forecast["pop"]
+        temperature = forecast["main"]["temp"] - 273.15  # convertion K to C
 
-chosen_weather
+        chosen_weather.append(
+            {
+                "city": city,
+                "dt_object": dt_object,
+                "latitude": latitude,
+                "longitude": longitude,
+                "main_weather": main_weather,
+                "precipitation": precipitation,
+                "temperature": temperature,
+            }
+        )
+
 
 # we tranform our data in dataframe
 
 city_weather = pd.DataFrame.from_records(chosen_weather)
-city_weather
 
 # tranform the infomation about city weather by days and not anymore by each 3h
 
-city_weather['latitude'] = city_weather['latitude'].astype(float)
-city_weather['longitude'] = city_weather['longitude'].astype(float)
+city_weather["latitude"] = city_weather["latitude"].astype(float)
+city_weather["longitude"] = city_weather["longitude"].astype(float)
 
-city_weather_by_day = city_weather.groupby([city_weather['city'],city_weather['dt_object'].dt.date]).agg({ 
-    'main_weather' : [pd.Series.mode], 
-    'prepcipitation' : ['sum'], 
-    'temperature' : ['mean'],
-    'latitude' : ['mean'],
-    'longitude' : ['mean']}).reset_index()
+city_weather_by_day = (
+    city_weather.groupby([city_weather["city"], city_weather["dt_object"].dt.date])
+    .agg(
+        {
+            "main_weather": [pd.Series.mode],
+            "precipitatoin": ["sum"],
+            "temperature": ["mean"],
+            "latitude": ["mean"],
+            "longitude": ["mean"],
+        }
+    )
+    .reset_index()
+)
 
 city_weather_by_day.columns = city_weather_by_day.columns.droplevel(1)
 
+# creation of a file with the top 5 to feed the booking scrapping
+
+top_city = (
+    city_weather_by_day.groupby("city", sort=False)
+    .mean()
+    .reset_index()
+    .sort_values(["precipitation", "temperature"], ascending=[True, False])
+)
+
+Top_city = top_city["city"]
+Top_5_city = Top_city.head(5)
+
+path = "data/"
+os.chdir(path)
+
+Top_5_city.to_csv("top_5_city_name.csv", index=False)
+
+print(city_weather_by_day.head())
+print(Top_5_city.head())
+
+# Filter on top 5cities
+
+city_weather_top_5 = city_weather_by_day[city_weather_by_day["city"].isin(Top_5_city)]
+
+print(city_weather_by_day.head())
+
+
 # send the data in our S3
 
-session = boto3.Session(aws_access_key_id=os.getenv("AWS_ACCESS_LEY_ID"), 
-                        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
+session = boto3.Session(
+    aws_access_key_id=os.getenv("AWS_ACCESS_LEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+)
 
 
 s3 = session.resource("s3")
 
 bucket = s3.create_bucket(Bucket="booking-scapping")
 
-csv = city_weather_by_day.to_csv()
+csv = city_weather_top_5.to_csv()
 
 put_object = bucket.put_object(Key="city_weather_by_day.csv", Body=csv)
 
-# creation of a file with the top 5 to feed the booking scrapping
 
-top_city = city_weather_by_day.groupby('city', sort=False).mean().reset_index().sort_values(['prepcipitation','temperature'], ascending = [True, False])
-
-Top_city = top_city['city']
-Top_5_city = Top_city.head(5)
-
-path = 'data/'
-os.chdir(path)
-
-Top_5_city.to_csv('top_5_city_name.csv', index=False)
-
-print('We got the top 5 cities in our data folder !')
+print("We got the top 5 cities in our data folder !")
--- 2-Scrap.py
+++ 2-Scrap.py
@@ -1,37 +1,46 @@
-import os 
-import pandas as pd 
+import os
+import pandas as pd
 import logging
 import boto3
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
-class QuotesSpider(scrapy.Spider):
 
+class QuotesSpider(scrapy.Spider):
     # Name of your spider
     name = "booking_scrap"
 
-    # Url to start your spider from 
+    # Url to start your spider from
     start_urls = [
-      "https://www.booking.com/searchresults.fr.html?ss=nimes&ssne=Paris&ssne_untouched=Paris&efdco=1&label=gen173nr-1FCAEoggI46AdIM1gEaEaIAQGYAQ24AQfIAQzYAQHoAQH4AQuIAgGoAgO4AperqZ8GwAIB0gIkNTVlM2Q0MzgtM2E5Ny00OTI3LTliZTUtOWI2ZGU2Y2UzZDAw2AIG4AIB&sid=1f5200ba9085196426266e923706b5c3&aid=304142&lang=fr&sb=1&src_elem=sb&src=searchresults&group_adults=2&no_rooms=1&group_children=0&sb_travel_purpose=leisure",
+        "https://www.booking.com/searchresults.fr.html?ss=nimes&ssne=Paris&ssne_untouched=Paris&efdco=1&label=gen173nr-1FCAEoggI46AdIM1gEaEaIAQGYAQ24AQfIAQzYAQHoAQH4AQuIAgGoAgO4AperqZ8GwAIB0gIkNTVlM2Q0MzgtM2E5Ny00OTI3LTliZTUtOWI2ZGU2Y2UzZDAw2AIG4AIB&sid=1f5200ba9085196426266e923706b5c3&aid=304142&lang=fr&sb=1&src_elem=sb&src=searchresults&group_adults=2&no_rooms=1&group_children=0&sb_travel_purpose=leisure",
     ]
 
-    #we create a loop over our top 5 cities 
+    # we create a loop over our top 5 cities
     def parse(self, response):
-        top_5_city = pd.read_csv('data/top_5_city_name.csv')
-        cities = top_5_city['city'].values.tolist()
+        top_5_city = pd.read_csv("data/top_5_city_name.csv")
+        cities = top_5_city["city"].values.tolist()
         for city in cities:
-            yield scrapy.FormRequest.from_response(response, formdata={'ss': city}, callback=self.parse_city)
+            yield scrapy.FormRequest.from_response(
+                response, formdata={"ss": city}, callback=self.parse_city
+            )
 
-    # we take the information og the top 20 hotels per cities 
+    # we take the information og the top 20 hotels per cities
     def parse_city(self, response):
         i = 0
         for path in response.xpath('//*[@data-testid="property-card"]'):
-            url = path.xpath('div[1]/div[1]/div/a').attrib['href']
+            url = path.xpath("div[1]/div[1]/div/a").attrib["href"]
             data = {
-                'hotel name': path.xpath('div[1]/div[2]/div/div/div/div[1]/div/div[1]/div/h3/a/div[1]/text()').get(),
-                'Url to its booking.com page': url,
-                'Score': path.xpath('div[1]/div[2]/div/div/div/div[2]/div/div[1]/div/a/span/div/div[1]/text()').get(),
-                'Text description': path.xpath('div/div[2]/div/div/div/div/div/div[3]/text()').get() or path.xpath('div/div[2]/div/div/div/div/div/div[4]/text()').get(),
+                "hotel name": path.xpath(
+                    "div[1]/div[2]/div/div/div/div[1]/div/div[1]/div/h3/a/div[1]/text()"
+                ).get(),
+                "Url to its booking.com page": url,
+                "Score": path.xpath(
+                    "div[1]/div[2]/div/div/div/div[2]/div/div[1]/div/a/span/div/div[1]/text()"
+                ).get(),
+                "Text description": path.xpath(
+                    "div/div[2]/div/div/div/div/div/div[3]/text()"
+                ).get()
+                or path.xpath("div/div[2]/div/div/div/div/div/div[4]/text()").get(),
             }
 
             yield response.follow(url, callback=self.parse_products, meta=data)
@@ -39,51 +48,71 @@ class QuotesSpider(scrapy.Spider):
             if i == 20:
                 break
 
-    # we go into the url of each hotels to take their localisation 
+    # we go into the url of each hotels to take their localisation
     def parse_products(self, response):
         return {
             **response.meta,
-            'latlng': response.xpath('//*[@id="hotel_sidebar_static_map"]').attrib['data-atlas-latlng'],
+            "latlng": response.xpath('//*[@id="hotel_sidebar_static_map"]').attrib[
+                "data-atlas-latlng"
+            ],
         }
+
 
 # Name of the file where the results will be saved
 filename = "booking.json"
 
-# If file already exists, delete it before crawling (because Scrapy will 
+# If file already exists, delete it before crawling (because Scrapy will
 # concatenate the last and new results otherwise)
-if filename in os.listdir('data/'):
-        os.remove('data/' + filename)
+if filename in os.listdir("data/"):
+    os.remove("data/" + filename)
 
 
 # Declare a new CrawlerProcess with some settings
 ## USER_AGENT => Simulates a browser on an OS
-## LOG_LEVEL => Minimal Level of Log 
-## FEEDS => Where the file will be stored 
-process = CrawlerProcess(settings = {
-    'USER_AGENT': 'Chrome/97.0',
-    'LOG_LEVEL': logging.INFO,
-    "FEEDS": {
-        'data/' + filename : {"format": "json"},
+## LOG_LEVEL => Minimal Level of Log
+## FEEDS => Where the file will be stored
+process = CrawlerProcess(
+    settings={
+        "USER_AGENT": "Chrome/97.0",
+        "LOG_LEVEL": logging.INFO,
+        "FEEDS": {
+            "data/" + filename: {"format": "json"},
+        },
     }
-})
+)
 
 # Start the crawling using the spider you defined above
 process.crawl(QuotesSpider)
 process.start()
 
+# clean the data before send it to S3
 
-#Send the data to my S3 
+hotel = pd.read_json("data/booking.json")
 
-session = boto3.Session(aws_access_key_id=os.getenv("AWS_ACCESS_LEY_ID"), 
-                        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
+hotel[["lat", "lon"]] = hotel["latlng"].str.split(",", 1, expand=True)
+hotel = hotel.drop(
+    ["depth", "download_timeout", "download_slot", "latlng", "download_latency"], axis=1
+)
+
+hotel["Score"] = hotel["Score"].replace({",": "."}, regex=True)
+
+hotel["Score"] = hotel["Score"].astype(float)
+hotel["lat"] = hotel["lat"].astype(float)
+hotel["lon"] = hotel["lon"].astype(float)
+
+
+# Send the data to my S3
+
+session = boto3.Session(
+    aws_access_key_id=os.getenv("AWS_ACCESS_LEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+)
 s3 = session.resource("s3")
 
 bucket = s3.create_bucket(Bucket="booking-scapping")
 
-df = pd.read_json('data/booking.json')
-print(df.head())
-csv = df.to_csv()
+csv = hotel.to_csv()
 
 bucket.put_object(Key="hotels_info.csv", Body=csv)
 
-print('We got the top hotels and their infos in our data folder !')
+print("We got the top hotels and their infos in our data folder !")
